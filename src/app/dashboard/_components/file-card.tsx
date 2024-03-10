65b86399168782373156f9be8500639b2427c2f4
priyanshu9241 <97443584+priyanshu9241@users.noreply.github.com>
2024-03-10 19:24:16 +0530

    fixed image preview

--- src/app/dashboard/_components/file-card.tsx
+++ src/app/dashboard/_components/file-card.tsx
@@ -11,14 +11,13 @@ import { formatRelative } from "date-fns";
 import { useState } from "react";
 
 import { Doc } from "../../../../convex/_generated/dataModel";
-import { FileTextIcon, GanttChartIcon, ImageIcon,X } from "lucide-react";
+import { FileTextIcon, GanttChartIcon, ImageIcon, X } from "lucide-react";
 import { ReactNode } from "react";
 import { useQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
 import Image from "next/image";
 import { FileCardActions, getFileUrl } from "./file-actions";
 import { useToast } from "@/components/ui/use-toast";
-
 export function FileCard({
   file,
 }: {
@@ -27,69 +26,90 @@ export function FileCard({
   const userProfile = useQuery(api.users.getUserProfile, {
     userId: file.userId,
   });
-  
+
   const typeIcons = {
     image: <ImageIcon />,
+
     pdf: <FileTextIcon />,
+
     csv: <GanttChartIcon />,
   } as Record<Doc<"files">["type"], ReactNode>;
+
   const [isImageOpen, setIsImageOpen] = useState(false);
   const { toast } = useToast();
+  const fileUrl = getFileUrl(file.fileId);
+
   return (
     <Card className="flex-col cursor-pointer">
       {isImageOpen && (
-        <div className="absolute h-full my-auto w-full top-0 bottom-0 right-0 left-0 backdrop-blur-xl z-[1000]
-        flex justify-center item-center">
-        <X onClick={() => setIsImageOpen(false)} className="absolute top-0 right-0 w-10 h-10 cursor-pointer" />
-          <img
-          onClick={() => setIsImageOpen(true)}
-            alt={file.name}
-            className="  aspect-auto mx-auto"
-            src={getFileUrl(file.fileId)}
+        <div className="fixed inset-0 z-50 bg-black bg-opacity-80  backdrop-blur-md flex justify-center items-center">
+          <X
+            onClick={() => setIsImageOpen(false)}
+            className="absolute top-2 right-2 text-white cursor-pointer"
           />
 
-
+          <img
+            alt={file.name}
+            className="max-w-full max-h-full"
+            src={fileUrl}
+          />
         </div>
       )}
+
       <CardHeader className="relative">
         <CardTitle className="flex gap-2 text-base font-normal">
           <div className="flex justify-center">{typeIcons[file.type]}</div>
+
           {file.name}
         </CardTitle>
+
         <div className="absolute top-2 right-2">
           <FileCardActions isFavorited={file.isFavorited} file={file} />
         </div>
       </CardHeader>
+
       <CardContent className="flex justify-center items-center h-[200px]">
         {file.type === "image" && (
           <Image
-          onClick={() => setIsImageOpen(true)}
+            onClick={() => setIsImageOpen(true)}
             width="1000"
             height="300"
             alt={file.name}
             className="w-full h-full object-cover aspect-square"
-            src={getFileUrl(file.fileId)}
+            src={fileUrl}
           />
         )}
 
         {file.type === "csv" && (
-          <GanttChartIcon onClick={()=>{
+          <GanttChartIcon
+            onClick={() => {
               toast({
-                  variant: "destructive",
-                  title: "No support",
-                  description: "Currently we do not support viewing these file types",
-                });
-          }} className="w-full h-full sm:w-[60px] sm:h-[60px]" />
+                variant: "destructive",
+
+                title: "No support",
+
+                description:
+                  "Currently we do not support viewing these file types",
+              });
+            }}
+            className="w-full h-full sm:w-[60px] sm:h-[60px]"
+          />
         )}
 
         {file.type === "pdf" && (
-          <FileTextIcon onClick={()=>{
+          <FileTextIcon
+            onClick={() => {
               toast({
-                  variant: "destructive",
-                  title: "No support",
-                  description: "Currently we do not support viewing these file types",
-                });
-          }} className="w-full h-full sm:w-[60px] sm:h-[60px]" />
+                variant: "destructive",
+
+                title: "No support",
+
+                description:
+                  "Currently we do not support viewing these file types",
+              });
+            }}
+            className="w-full h-full sm:w-[60px] sm:h-[60px]"
+          />
         )}
       </CardContent>
 
@@ -97,10 +117,13 @@ export function FileCard({
         <div className="flex gap-2 text-xs text-gray-700 w-40 items-center">
           <Avatar className="w-6 h-6">
             <AvatarImage src={userProfile?.image} />
+
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
+
           {userProfile?.name}
         </div>
+
         <div className="text-xs text-gray-700">
           Uploaded on {formatRelative(new Date(file._creationTime), new Date())}
         </div>
